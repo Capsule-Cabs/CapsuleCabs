d380375e86e1e943c4de615fad2a6ec37bc20979
VISHALSOLANKI09 <Vishal.Solanki@GeminiSolutions.com>
2026-01-27 21:35:25 +0530

    chore: added android and ios setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.capsulecabs.app',
+  appName: 'CapsuleCabs',
+  webDir: 'dist'
+};
+
+export default config;
