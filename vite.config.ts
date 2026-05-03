55aa31693e6ddd158885c0644f949e14c5c183cc
Vishal Solanki <vishalsolanki14002@gmail.com>
2026-05-03 23:18:53 +0530

    chore: fixing vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { componentTagger } from "lovable-tagger";
 import prerender from "vite-plugin-prerender";
+
+// ✅ FIX for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -13,7 +19,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
 
-    // ✅ FIXED CONFIG
     prerender({
       staticDir: path.resolve(__dirname, "dist"),
       routes: ["/"],
