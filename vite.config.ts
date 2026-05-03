dcf344346ce00fb5e24b487350e35900caf927ac
Vishal Solanki <vishalsolanki14002@gmail.com>
2026-05-03 23:23:45 +0530

    chore: removing rerender

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { componentTagger } from "lovable-tagger";
-import prerender from "vite-plugin-prerender";
-
-// ✅ FIX for __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -18,12 +11,6 @@ export default defineConfig(({ mode }) => ({
 
   plugins: [
     react(),
-
-    prerender({
-      staticDir: path.resolve(__dirname, "dist"),
-      routes: ["/"],
-    }),
-
     mode === "development" && componentTagger(),
   ].filter(Boolean),
 
@@ -34,6 +21,6 @@ export default defineConfig(({ mode }) => ({
   },
 
   build: {
-    target: "es2015",
+    target: "es2015", // helps older browsers like TVs
   },
 }));
